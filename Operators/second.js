1dfe6a238da1905cad12952a0b7d8da3873a830a
Kavitha Sai Prasanth <166430594+saiprasanth201@users.noreply.github.com>
2024-10-15 05:38:50 +0530

    Add files via upload

---
+++ Operators/second.js
@@ -0,0 +1,89 @@
+// Arithemetic operators
+
+// let a = 5;
+// let b = 2;
+
+// console.log(" a = ",a ,"b =",b);
+// console.log(" a + b ",a+b);
+// console.log(" a * b ",a*b);
+// console.log(" a / b ",a/b);
+// console.log(" a % b ",a%b);
+// console.log(" a ** b ",a**b);
+
+// Comparision Operators
+
+// Equal to ==,
+// Equal to and type ===,
+// not equal to !=,
+// not Equal to and type ===
+// >,>=,<,<=
+
+//Logical operators 
+
+// Logical AND  &&;
+// Logical OR   ||;
+// Logical NOT   !;
+
+
+// Conditional statements:
+
+// // if statements
+
+// let mode = "light";
+// let color;
+
+// if (mode === "dark"){
+//     color = "black";
+// }
+// if (mode === "light"){
+//     color = "white";
+// }
+// console.log(color);
+
+// if else statements
+
+// let color;
+// if(mode === "dark-mode")
+//     {
+//     color = "black";
+//     } else {
+//      color = "white";
+//     }
+
+//check if a number is odd or even 
+
+// let num = 65;
+// if (num % 2 === 0){
+//     console.log(num,"is even");
+// }else{
+//     console.log(num,"is odd");
+// }
+
+//check if a number is multiple of 5 using prompt :
+
+// let num = prompt("Enter a number : ")
+
+// if(num % 5 === 0){
+//     console.log(num, "is a multiple of 5" );
+// }else{
+//     console.log(num, "is a not multiple of 5" );
+// }
+
+// write a code which can give grades according to scores :
+
+// let grades;
+// let marks = prompt("enter your marks : ");
+// if (marks <= 100 && marks >= 90){
+//     grades = "A";
+// }else if (marks <= 89 && marks >= 70){
+//     grades = "B";
+// }else if (marks <= 69 && marks >= 60){
+//     grades = "C";
+// }else if (marks <= 59 && marks >= 50){
+//     grades = "D";
+// }else if (marks <= 49 && marks >= 0){
+//     grades = "F";
+// }else{
+//     grade = "not defined";
+// }
+// console.log("your grade was :",grades);
