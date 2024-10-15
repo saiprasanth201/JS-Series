cb119654ebb031d082e16644e2fd75fef1afe30f
Kavitha Sai Prasanth <166430594+saiprasanth201@users.noreply.github.com>
2024-10-15 05:36:31 +0530

    Add files via upload

---
+++ Variables&Datatypes/first.js
@@ -0,0 +1,32 @@
+//Variables:
+
+let fullName = "Tony Stark";
+let age = 24;
+let price = 99.99;
+var x = null;
+const y = undefined;
+console.log(price);
+
+//Data Types:
+
+//primitive : numbers string boolean undefined null bigint symbol.
+//non primitive : objects :
+//typeof age => 'number'.
+
+const product = {
+    title: "Ball Pen",
+    rating: 4,
+    offer: 5,
+    price:270
+};
+console.log(product);
+
+const Profile = {
+    ID: "saiprasanth201",
+    isFollow:true,
+    posts: 6,
+    followers:603,
+    following:489,
+    bio: "music lover"
+};
+console.log(Profile);
