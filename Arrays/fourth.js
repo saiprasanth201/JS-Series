0a263f0d195fa62a55c5e782ea24ddd58d98c092
Kavitha Sai Prasanth <166430594+saiprasanth201@users.noreply.github.com>
2024-10-16 15:56:11 +0530

    Add files via upload

---
+++ Arrays/fourth.js
@@ -0,0 +1,43 @@
+// let marks = [0,5,10,15,20];
+// console.log(marks);
+// console.log(marks.length);
+
+// console.log(marks[3],marks[2],marks[1],marks[4],marks[0]);
+
+// //Array using for loops:
+// for(let idx=0;idx<=marks.length;idx++){
+//     console.log(marks[idx]);
+// }
+
+// //Array using for-of loops:
+// let cities = ["Mumbai","Chennai","Kolkata","Delhi","Bangalore"];
+// for(let city of cities ){
+//     console.log(city.toUpperCase());
+// }
+
+
+// let marks = [85,97,44,37,76,60];
+// // let sum = marks[0]+marks[1]+marks[2]+marks[3]+marks[4]+marks[5];
+// let sum = for(let idx=0;idx<= marks.length;idx++){
+//     console.log(marks[idx]);
+// }
+// let totalNum= marks.length;
+// let avg= sum/totalNum;
+
+// let foodItems = ["Potato","Tomato","Litchi","apple"];
+// console.log(foodItems);
+// foodItems.push("chips");
+// console.log(foodItems);
+// foodItems.pop();
+// console.log(foodItems);
+// foodItems.pop();
+// console.log(foodItems.toString());
+
+// let company = ["Bloomberg","Microsoft","Uber","Google","Ibm","Netflix"];
+// console.log(company);
+// company.shift();
+// console.log(company);
+// company.splice(1,1,"Ola");
+// console.log(company);
+// company.push("Amazon");
+// console.log(company);
