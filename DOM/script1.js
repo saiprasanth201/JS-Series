a57a8f124e58a4fa0c6e68b6095395ed0f07f044
Kavitha Sai Prasanth <166430594+saiprasanth201@users.noreply.github.com>
2024-10-25 22:34:57 +0530

    Add files via upload

---
+++ DOM/script1.js
@@ -0,0 +1,19 @@
+// console.log("Hello !");
+// alert("hi prasanth!");
+
+// let heading = document.getElementById("heading");
+// console.dir(heading);
+
+//query selectors
+// let firstEl = document.querySelector("p")//1st element
+// console.dir(firstEl);
+
+// let allEl = document.querySelectorAll("p")//all elements
+// console.dir(allEl);
+
+// let h2 = document.querySelector("h2");
+// console.dir(h2.innerText);
+
+// h2.innerText = h2.innerText + "from apna college students"; //concatenate
+
+//let div1 = document.
